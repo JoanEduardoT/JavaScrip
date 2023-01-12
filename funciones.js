5bc03caf0082262339caf1c2c57fdb1fa0d0bd0b
larisalm <larisalopezm17@gmail.com>
2023-01-12 14:57:04 -0600

    Funciones

---
+++ funciones.js
@@ -0,0 +1,32 @@
+/* const nombre= prompt('Da tu nombre');
+
+const saludo = (nombre)=>{
+    console.log('Holaaaaa'+ nombre);
+};
+
+function saludo2(){
+    console.log('saludo 2')
+};
+
+saludo(nombre); */
+
+const vocal =prompt('Escribe una vocal');
+const letra =(vocal)=>{
+
+if (vocal == 'A'){
+    console.log('Pusiste la letra A')
+} else if (vocal == 'B') {
+    console.log('Pusiste la letra B')
+} else if (vocal == 'D') {
+    console.log('Pusiste la letra C')
+} else if (vocal == 'C') {
+    console.log('Pusiste la letra D')
+} else if (vocal == 'E') {
+    console.log('Pusiste la letra E')
+} else  {
+    console.log('No se encontro nada')
+} 
+
+}
+
+letra(vocal);
--- variables.js
+++ variables.js
@@ -1,1 +1,14 @@
 console.log("123 HOLA");
+
+var nombres = 'Joan Eduardo';
+var apellidos = 'Torres Espindola';
+var edad = 20;
+var altura= 1.76;
+var peso= 75;
+var correo= 'joane.torresespindola@gmail.com';
+var carrera ='TI';
+var grupo= '4-1';
+
+
+//        console.log('Hola desde javascrip mi nombre es '+nombre+' y mi edad es '+edad);
+console.log(`Hola desde javascrip mi nombre es ${nombres} y apellidos son ${apellidos}, soy de la carrera ${carrera} del grupo ${grupo}, mi correo es ${correo}, mi edad es ${edad}, mido ${altura} y peso ${peso}`);
