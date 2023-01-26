3265bc2f97021aa151b4b61e3ee073fe8fa215c4
larisalm <larisalopezm17@gmail.com>
2023-01-26 14:49:09 -0600

    CRUD

--- vue/carritoObj.js
+++ vue/carritoObj.js
@@ -1,10 +1,42 @@
 Vue.createApp({
     data(){
         return {
+            nombre:'',
+            precio:'',
+            cantidad:'',
+            fecha:'',
+            peso:'',
+            codigo:'',
 
+            productos:[
+
+
+                /* {
+                    nombre:"Piña",
+                    precio: 20.50,
+                    cantidad: 3
+                },
+                {
+                    nombre:"Manzana",
+                    precio: 3.50,
+                    cantidad: 40
+                }, */
+
+            ]
         }
     },
     methods:{
+        agregar(){
+            const producto={
+                nombre:this.nombre,
+                precio:this.precio,
+                cantidad:this.cantidad,
+                fecha:this.fecha,
+                peso:this.peso,
+                codigo:this.codigo,
+            }
+            this.productos.push(producto);
+        }
         
     }
 }).mount('#app')
