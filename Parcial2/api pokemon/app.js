000d2a7aa3325db39e4f318f95ee66a94c4b7c6d
larisalm <larisalopezm17@gmail.com>
2023-02-21 09:28:11 -0600

    API pokemon 200

--- Parcial2/api pokemon/app.js
+++ Parcial2/api pokemon/app.js
@@ -1,23 +1,38 @@
 Vue.createApp({
-    data(){
+    data() {
         return {
-            pokemon:null
-        
+            pokemones: []
+
         }
     },
-    methods:{
-        async getPokemon(){
+    mounted() {
+        this.getPokemon
+    }
+    ,
+    methods: {
+        async getPokemon() {
             //GET --- obtener informacion
             //POST --- para crear recursos
             //PUT --- modificar recursos
             //DELETE --- elimirar recursos
-            const random = Math.floor(Math.random() * 1008);
-           const response = await fetch('https://pokeapi.co/api/v2/pokemon/'+random,{
-                method:'GET'
-            });
 
-            const data = await response.json();
-            this.pokemon= data
+            /*         const random = Math.floor(Math.random() * 1008);
+                       const response = await fetch('https://pokeapi.co/api/v2/pokemon/'+random,{
+                            method:'GET'
+                        }); */
+            var i = 1
+            while (i <= 200) {
+
+                const response = await fetch('https://pokeapi.co/api/v2/pokemon/' + i, {
+                    method: 'GET'
+                })
+                    ;
+                const pokemon = await response.json();
+                this.pokemones.push(pokemon);
+                i++
+            }
+
+
         }
     }
 }).mount('#app')
